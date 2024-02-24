707b795ff02484786c5b73f0b6c2beecc31d4a28
Estherbab <153003348+Estherbab@users.noreply.github.com>
2024-02-24 15:27:10 +0000

    Added code for Manager inherited properties & methods

--- starter/lib/Manager.js
+++ starter/lib/Manager.js
@@ -1,1 +1,16 @@
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+const Employee = require("./Employee")    
+class Manager extends Employee{                                          // My manager class can inherit all the stuff that is built into my employee class
+constructor(name, id, email, officeNumber){
+    super(name, id, email)                                              // inherit the behaviours of these from the parent(Employee)
+    this.officeNumber = officeNumber
+}
+getofficeNumber(){
+    return this.officeNumber
+}
+getRole(){
+    return "Manager"
+}
+}
+
+module.exports=Manager
